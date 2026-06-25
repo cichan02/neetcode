2ccc7d3c08558f43f493263795e6f0bc3e3ac47a
cichan <90219032+cichan02@users.noreply.github.com>
2026-06-25 09:56:24 +0300

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0, minPrice = prices[0];
+        for(int price: prices) {
+            maxProfit = Math.max(maxProfit, price - minPrice);
+            minPrice = Math.min(minPrice, price);
+        }
+        return maxProfit;
+    }
+}
