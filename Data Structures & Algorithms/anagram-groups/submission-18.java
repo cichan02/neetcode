6df7f4300bcb61a6ad4cabe4d85abc4f66115271
cichan <90219032+cichan02@users.noreply.github.com>
2026-06-26 08:39:39 +0300

    Add: anagram-groups - submission-18

---
+++ Data Structures & Algorithms/anagram-groups/submission-18.java
@@ -0,0 +1,13 @@
+class Solution {
+        public List<List<String>> groupAnagrams(String[] strs) {
+            Map<String, List<String>> map = new HashMap<>();
+            for (String str : strs) {
+                char[] charArray = str.toCharArray();
+                Arrays.sort(charArray);
+                String key = new String(charArray);
+                map.putIfAbsent(key, new ArrayList<>());
+                map.get(key).add(str);
+            }
+            return new ArrayList<>(map.values());
+        }
+}
