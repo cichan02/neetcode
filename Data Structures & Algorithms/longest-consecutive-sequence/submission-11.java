36e10d995e99e484a249be7d48b3468fe1b44a02
cichan <90219032+cichan02@users.noreply.github.com>
2026-07-01 11:07:32 +0300

    Add: longest-consecutive-sequence - submission-11

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-11.java
@@ -0,0 +1,68 @@
+   class UnionFind {
+        private int[] parent;
+        private int[] rank;
+        private int longest;
+
+        public UnionFind(int n) {
+            this.parent = new int[n];
+            this.rank = new int[n];
+            this.longest = 1;
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                rank[i] = 1;
+            }
+        }
+
+        public int find(int p) {
+            if (parent[p] != p) {
+                parent[p] = find(parent[p]);
+            }
+            return parent[p];
+        }
+
+        private void union(int p, int q) {
+            int rootP = find(p);
+            int rootQ = find(q);
+            if (rootP == rootQ) {
+                return;
+            }
+            if (rank[rootP] < rank[rootQ]) {
+                parent[rootP] = rootQ;
+                rank[rootQ] += rank[rootP];
+                longest = Math.max(longest, rank[rootQ]);
+            } else {
+                parent[rootQ] = rootP;
+                rank[rootP] += rank[rootQ];
+                longest = Math.max(longest, rank[rootP]);
+            }
+        }
+    }
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return 1;
+
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            numToIndex.put(nums[i], i);
+        }
+
+        UnionFind uf = new UnionFind(nums.length);
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+
+            if (numToIndex.get(num) != i) {
+                continue;
+            }
+
+            if (numToIndex.containsKey(num - 1)) {
+                int j = numToIndex.get(num - 1);
+                uf.union(i, j);
+            }
+        }
+
+        return uf.longest;
+    }
+}
