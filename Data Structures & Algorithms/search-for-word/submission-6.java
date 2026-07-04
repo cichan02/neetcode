14746cf2bc56ac4026b77c96af73d0a8c081a5e3
cichan <90219032+cichan02@users.noreply.github.com>
2026-07-04 11:11:55 +0300

    Add: search-for-word - submission-6

---
+++ Data Structures & Algorithms/search-for-word/submission-6.java
@@ -0,0 +1,38 @@
+class Solution {
+    private static int COLS;
+    private static int ROWS;
+
+    public boolean exist(char[][] board, String word) {
+        COLS = board.length;
+        ROWS = board[0].length;
+        for (int i = 0; i < COLS; i++) {
+            for (int j = 0; j < ROWS; j++) {
+                if (backtrack(board, i, j, word)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean backtrack(char[][] board, int i, int j, String word) {
+        if (word.isEmpty()) {
+            return true;
+        }
+
+        if (i < 0 || i >= COLS ||
+            j < 0 || j >= ROWS ||
+            board[i][j] != word.charAt(0)) {
+            return false;
+        }
+
+        board[i][j] = ' ';
+        char temp = word.charAt(0);
+        boolean res = backtrack(board, i - 1, j, word.substring(1)) ||
+            backtrack(board, i + 1, j, word.substring(1)) ||
+            backtrack(board, i, j - 1, word.substring(1)) ||
+            backtrack(board, i, j + 1, word.substring(1));
+        board[i][j] = temp;
+        return res;
+    }
+}
