a284df9749e6fd61474646f9f66d63a3c19c2340
cichan <90219032+cichan02@users.noreply.github.com>
2026-07-04 10:12:39 +0300

    Add: longest-consecutive-sequence - submission-16

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-16.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        int res = 0;
+        for (int num: nums) {
+            if (!set.contains(num-1)) {
+                int l = 1;
+                while (set.contains(num + l)) {
+                    l++;
+                }
+                res = Math.max(res, l);
+            }
+        }
+        return res;
+    }
+}
