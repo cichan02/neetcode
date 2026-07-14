89541fcb766cec7c84e5af5bcd1d8a7df6351c5c
cichan <90219032+cichan02@users.noreply.github.com>
2026-07-14 20:28:01 +0200

    Add: palindrome-linked-list - submission-0

---
+++ Data Structures & Algorithms/palindrome-linked-list/submission-0.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode medium = slow.next, prev = null;
+        slow.next = null;
+        while (medium != null) {
+            ListNode next = medium.next;
+            medium.next = prev;
+            prev = medium;
+            medium = next;
+        }
+
+        ListNode first = head, second = prev;
+        while (second != null && first != null) {
+            if (first.val != second.val) {
+                return false;
+            }
+            second = second.next;
+            first = first.next;
+        }
+
+        return true;
+    }
+}
