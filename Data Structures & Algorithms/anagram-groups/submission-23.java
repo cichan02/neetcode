3b20bb0e486a3ad46b161801eb6f40ad45f2eaee
cichan <90219032+cichan02@users.noreply.github.com>
2026-07-21 17:00:46 +0300

    Add: anagram-groups - submission-23

---
+++ Data Structures & Algorithms/anagram-groups/submission-23.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> group = HashMap.newHashMap(strs.length);
+        for (String s: strs) {
+            int[] freq  = new int[26];
+            for (char c: s.toCharArray()) {
+                freq[c - 'a']++;
+            }
+            String key = Arrays.toString(freq);
+            group.putIfAbsent(key, new ArrayList<>());
+            group.get(key).add(s);
+        }
+        return group.values().stream().toList();
+    }
+}
