047baa64664ef053fac5971af247b619db410b61
cichan <90219032+cichan02@users.noreply.github.com>
2026-07-27 23:15:03 +0300

    Add: contains-duplicate-ii - submission-10

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-10.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> uniques = HashSet.newHashSet(nums.length);
+        for (int l = 0, r = 0; r < nums.length; r++) {
+            if (uniques.contains(nums[r])) {
+                return true;
+            }
+
+            uniques.add(nums[r]);
+
+            if (r - l + 1 > k) {
+                uniques.remove(nums[l++]);
+            }
+        }
+        return false;
+    }
+}
