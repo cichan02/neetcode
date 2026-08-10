63941e4941b29ff90153b2d2bfd8335c1de373b7
cichan <90219032+cichan02@users.noreply.github.com>
2026-08-11 00:14:10 +0300

    Add: 4sum - submission-10

---
+++ Data Structures & Algorithms/4sum/submission-10.java
@@ -0,0 +1,49 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        Arrays.sort(nums);
+        kSum(4, res, new ArrayList<>(), 0, nums, target);
+
+        return res;
+    }
+
+    private void kSum(int k, List<List<Integer>> res, List<Integer> combination, int i, int[] nums, long target) {
+        if (k == 2) {
+            twoSum(res, combination, i, nums, target);
+            return;
+        }
+
+        for (int j = i; j < nums.length; j++) {
+            if (j > i && nums[j] == nums[j - 1]) {
+                continue;
+            }
+            combination.add(nums[j]);
+            kSum(k - 1, res, combination, j + 1, nums, target - nums[j]);
+            combination.removeLast();
+        }
+    }
+
+    private void twoSum(List<List<Integer>> res, List<Integer> combination, int i, int[] nums, long target) {
+        int l = i, r = nums.length - 1;
+        while (l < r) {
+            switch (Long.signum(nums[l] + nums[r] - target)) {
+                case 1 -> r--;
+                case -1 -> l++;
+                case 0 -> {
+                    List<Integer> tmp = Stream.concat(
+                            Stream.of(nums[l++], nums[r--]),
+                            combination.stream()
+                    ).toList();
+                    res.add(tmp);
+                    while (l < r && nums[l] == nums[l - 1]) {
+                        l++;
+                    }
+                    while (l < r && nums[r] == nums[r + 1]) {
+                        r--;
+                    }
+                }
+            }
+        }
+    }
+}
