7642f60660ef5a4cfa31815bff9efdf3d35b5e16
cichan <90219032+cichan02@users.noreply.github.com>
2026-08-11 23:25:27 +0300

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+
+            if (nums[m] == target) {
+                return m;
+            }
+
+            if (target < nums[m]) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return nums[l] == target ? l : -1;
+    }
+}
