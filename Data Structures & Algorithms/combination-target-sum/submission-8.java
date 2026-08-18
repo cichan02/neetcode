9263dfef2df6ad33621f6630c4af0e3283bbb518
cichan <90219032+cichan02@users.noreply.github.com>
2026-08-18 13:03:46 +0300

    Add: combination-target-sum - submission-8

---
+++ Data Structures & Algorithms/combination-target-sum/submission-8.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        backtrack(res, new ArrayList<>(), nums, 0, target);
+
+        return res;
+    }
+
+    private void backtrack(List<List<Integer>> res, List<Integer> cur, int[] nums, int i, int target) {
+        if (target == 0) {
+            res.add(List.copyOf(cur));
+            return;
+        }
+
+        for (int j = i; j < nums.length; j++) {
+            if (nums[j] > target) {
+                return;
+            }
+
+            cur.add(nums[j]);
+            backtrack(res, cur, nums, j, target - nums[j]);
+            cur.removeLast();
+        }
+    }
+}
