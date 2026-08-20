bba1fe8b3d408d84306804f6a5844f4aa8407732
cichan <90219032+cichan02@users.noreply.github.com>
2026-08-20 13:15:48 +0300

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int climbStairs(int n) {
+        int zero = 1, first = 1;
+        for (int i = 2; i < n+1; i++) {
+            int tmp = first;
+            first += zero;
+            zero = tmp;
+        }
+        return first;
+    }
+}
