749d1bbd138fe4764c3e9f4333ba96f9dae0728a
cichan <90219032+cichan02@users.noreply.github.com>
2026-08-21 16:44:17 +0300

    Add: longest-repeating-substring-with-replacement - submission-16

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-16.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        Map<Integer, Integer> freq = HashMap.newHashMap(s.length());
+        int maxL = 0, maxF = 0;
+        for (int l = 0, r = 0; r < s.length(); r++) {
+            int rc = s.charAt(r);
+            freq.merge(rc, 1, Integer::sum);
+            maxF = Math.max(maxF, freq.get(rc));
+
+            while ((r - l + 1) - maxF > k) {
+                int lc = s.charAt(l++);
+                freq.merge(lc, -1, Integer::sum);
+            }
+
+            maxL = Math.max(maxL, r - l + 1);
+        }
+        return maxL;
+    }
+}
