a5f0d22f9724e0cf89fde1fd641fa8c5af2fe8c3
cichan <90219032+cichan02@users.noreply.github.com>
2026-08-21 14:50:51 +0300

    Add: implement-prefix-tree - submission-7

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-7.java
@@ -0,0 +1,57 @@
+class PrefixTree {
+    private static final int K = 26;
+
+    private final Node root;
+
+    public PrefixTree() {
+        this.root = new Node();
+    }
+
+    public void insert(String word) {
+        Node cur = root;
+        for (char c: word.toCharArray()) {
+            int i = c - 'a';
+            if (cur.children[i] == null) {
+                cur.children[i] = new Node();
+            }
+            cur = cur.children[i];
+        }
+        cur.isTerminal = true;
+    }
+
+    public boolean search(String word) {
+        Node cur = root;
+        for (char c: word.toCharArray()) {
+            int i = c - 'a';
+            if (cur.children[i] == null) {
+                return false;
+            }
+            cur = cur.children[i];
+        }
+        return cur.isTerminal;
+    }
+
+    public boolean startsWith(String prefix) {
+        Node cur = root;
+        for (char c: prefix.toCharArray()) {
+            int i = c - 'a';
+            if (cur.children[i] == null) {
+                return false;
+            }
+            cur = cur.children[i];
+        }
+        return true;
+    }
+
+    private static final class Node {
+        private final Node[] children;
+
+        private boolean isTerminal;
+
+
+        public Node() {
+            this.children = new Node[K];
+            this.isTerminal = false;
+        }
+    }
+}
