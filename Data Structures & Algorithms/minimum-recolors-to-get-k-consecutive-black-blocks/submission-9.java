c502136a64418c8f140279109aee88b80c696bc6
cichan <90219032+cichan02@users.noreply.github.com>
2026-08-23 10:40:45 +0300

    Add: minimum-recolors-to-get-k-consecutive-black-blocks - submission-9

---
+++ Data Structures & Algorithms/minimum-recolors-to-get-k-consecutive-black-blocks/submission-9.java
@@ -0,0 +1,35 @@
+// Inside static (size of sliding window = k) count number of 'W' blocks
+// On each turn of sliding recound minimum
+
+// 1) check basic success
+// 2) if all blocks are 'B'?
+// 3) all blocks are 'W'?
+// 4) length of blocks = k;
+
+class Solution {
+    public int minimumRecolors(String blocks, int k) {
+        int countW = 0;
+
+        for (int i = 0; i < k; i++) {
+            if (blocks.charAt(i) == 'W') {
+                countW++;
+            }
+        }
+
+        int minR = countW;
+
+        for (int l = 0, r = k; r < blocks.length(); r++, l++) {
+            minR = Math.min(minR, countW);
+
+            if (blocks.charAt(l) == 'W') {
+                countW--;
+            }
+
+            if (blocks.charAt(r) == 'W') {
+                countW++;
+            }
+        }
+
+        return minR;
+    }
+}
