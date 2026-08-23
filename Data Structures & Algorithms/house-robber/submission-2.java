9d8ee7131568832ada553c839a3c9ddfea6186e4
cichan <90219032+cichan02@users.noreply.github.com>
2026-08-23 11:50:31 +0300

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int rob(int[] nums) {
+        int len = nums.length;
+        int[] res = new int[len+3];
+        for (int i = len-1; i >= 0; i--) {
+            res[i] = nums[i] + Math.max(res[i+2], res[i+3]);
+        }
+        return Math.max(res[0], res[1]);
+    }
+}
