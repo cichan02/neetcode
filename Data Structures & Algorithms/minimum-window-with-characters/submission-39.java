c2c4eca5c3dca6af7606b0b4dfc913d3bd4ccd38
cichan <90219032+cichan02@users.noreply.github.com>
2026-08-27 23:52:47 +0300

    Add: minimum-window-with-characters - submission-39

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-39.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        Map<Integer, Integer> freq = HashMap.newHashMap(t.length());
+        t.chars().forEach(i -> freq.merge(i, 1, Integer::sum));
+
+        Map<Integer, Integer> window = HashMap.newHashMap(s.length());
+        String res = "";
+        for (int l =0 , r = 0, have = 0; r < s.length(); r++) {
+            int rc = s.charAt(r);
+            window.merge(rc, 1, Integer::sum);
+            if (Objects.equals(window.get(rc), freq.get(rc))) {
+                have++;
+            }
+
+            while (freq.size() == have) {
+                if (r - l + 1 < res.length() || res.isEmpty()) {
+                    res = s.substring(l, r+1);
+                }
+
+                int lc = s.charAt(l++);
+                window.merge(lc, -1, Integer::sum);
+                if (freq.containsKey(lc) && window.get(lc) < freq.get(lc)) {
+                    have--;
+                }
+            }
+        }
+        return res;
+    }
+}
