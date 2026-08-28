714f611e8333ed671889306b4ff71a8a01089555
cichan <90219032+cichan02@users.noreply.github.com>
2026-08-29 00:03:51 +0300

    Add: house-robber-ii - submission-4

---
+++ Data Structures & Algorithms/house-robber-ii/submission-4.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rob(final int[] nums) {
+        int len = nums.length;
+        if (len == 1) {
+            return nums[0];
+        }
+        return Math.max(rob(nums, 0, len-1), rob(nums, 1, len));
+    }
+
+    private int rob(final int[] nums, final int start, final int stop) {
+        int zero = 0, first = 0, tmp;
+        for (int i = start; i < stop; i++) {
+            tmp = Math.max(nums[i] + zero, first);
+            zero = first;
+            first = tmp;
+        }
+        return first;
+    }
+}
