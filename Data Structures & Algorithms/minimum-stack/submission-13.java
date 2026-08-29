2c6cd815caee192c1d6c84a46c86de989cbfda9d
cichan <90219032+cichan02@users.noreply.github.com>
2026-08-29 19:02:07 +0300

    Add: minimum-stack - submission-13

---
+++ Data Structures & Algorithms/minimum-stack/submission-13.java
@@ -0,0 +1,46 @@
+class MinStack {
+        private final Deque<Long> deq;
+
+    private long min;
+
+    public MinStack() {
+        this.deq = new ArrayDeque<>();
+        this.min = Long.MAX_VALUE;
+    }
+
+    public void push(int val) {
+        if (deq.isEmpty()) {
+            this.deq.push(0L);
+            this.min = val;
+            return;
+        }
+        
+        deq.push(val - min);
+        if (val < min) {
+            this.min = val;
+        }
+    }
+
+    public void pop() {
+      
+
+        long pop = deq.removeFirst();
+
+        if (pop < 0) {
+            this.min = this.min - pop;
+        }
+    }
+
+    public int top() {
+        long top = deq.peekFirst();
+        if (top > 0) {
+            return (int) (this.min + top);
+        } else {
+            return (int) this.min;
+        }
+    }
+
+    public int getMin() {
+        return (int) this.min;
+    }
+}
