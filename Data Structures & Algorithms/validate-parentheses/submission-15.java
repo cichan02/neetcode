9c22584d1e3d6212f563a19683ab0186a73be052
cichan <90219032+cichan02@users.noreply.github.com>
2026-08-31 12:46:11 +0300

    Add: validate-parentheses - submission-15

---
+++ Data Structures & Algorithms/validate-parentheses/submission-15.java
@@ -0,0 +1,19 @@
+class Solution {
+    private static final Map P = Map.of(
+        '(', ')',
+        '{', '}',
+        '[', ']'
+    );
+
+    public boolean isValid(String s) {
+        Deque<Character> deq = new ArrayDeque<>(s.length());
+        for (Character c: s.toCharArray()) {
+            if (!deq.isEmpty() && c.equals(P.get(deq.peek()))) {
+                deq.pop();
+            } else {
+                deq.push(c);
+            }
+        }
+        return deq.isEmpty();
+    }
+}
