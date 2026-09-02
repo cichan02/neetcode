8a60bb9327622752dd5dccd49cb3e97e05da04c4
cichan <90219032+cichan02@users.noreply.github.com>
2026-09-02 16:07:30 +0300

    Add: isomorphic-strings - submission-5

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-5.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        final int len = s.length();
+
+        if (len != t.length()) return false;
+
+        final Map<Character, Character> st = HashMap.newHashMap(len);
+        final Map<Character, Character> ts = HashMap.newHashMap(len);
+
+        for (int i = 0; i < len; i++) {
+            char sc = s.charAt(i);
+            char tc = t.charAt(i);
+
+            if ((st.containsKey(sc) && st.get(sc) != tc) || (ts.containsKey(tc) && ts.get(tc) != sc)) {
+                return false;
+            }
+
+            st.put(sc, tc);
+            ts.put(tc, sc);
+        }
+
+        return true;
+    }
+}
