8c38922bed393c5a828cbfd90b0cd39bcbac45ea
cichan <90219032+cichan02@users.noreply.github.com>
2026-09-03 14:46:27 +0300

    Add: longest-palindromic-substring - submission-6

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-6.java
@@ -0,0 +1,48 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        CharSequence t = unify(s);
+
+        int[] p = oddManacher(t);
+
+        return longest(s, p).toString();
+    }
+
+    private CharSequence unify(final CharSequence s) {
+        final char del = '#';
+        final char[] t = new char[s.length() * 2 + 1];
+        t[0] = del;
+        for (int i = 0; i < s.length(); i++) {
+            t[i*2 + 1] = s.charAt(i);
+            t[i*2 + 2] = del;
+        }
+        return new String(t).intern();
+    }
+
+    private int[] oddManacher(final CharSequence s) {
+        final int len = s.length();
+        final int[] p = new int[len];
+        for (int l = 0, r = 0, i = 0; i < len; i++) {
+            p[i] = i > r ? 1 : Math.min(p[l+r-i], r - i + 1);
+            while (i - p[i] >= 0 && i + p[i] < len && s.charAt(i + p[i]) == s.charAt(i - p[i])) {
+                p[i]++;
+            }
+            if (i + p[i] - 1> r) {
+                r = i + p[i] - 1;
+                l = i - p[i] + 1;
+            }
+        }
+        return p;
+    }
+
+    private CharSequence longest(final CharSequence s, final int[] p) {
+        int resLen = 0, center_i = 0;
+        for (int i = 0; i < p.length; i++) {
+            if (p[i] > resLen) {
+                resLen = p[i];
+                center_i = i;
+            }
+        }
+        int resIdx = (center_i - resLen + 1) / 2;
+        return s.subSequence(resIdx, resIdx + resLen - 1);
+    }
+}
