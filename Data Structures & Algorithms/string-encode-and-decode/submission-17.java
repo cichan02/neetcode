07360f0cb640ad58eba8aa83ec2aeee51a5de475
cichan <90219032+cichan02@users.noreply.github.com>
2026-09-17 22:48:18 +0300

    Add: string-encode-and-decode - submission-17

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-17.java
@@ -0,0 +1,28 @@
+class Solution {
+    private static final char DEL = '#';
+
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String s: strs) {
+            sb.append(s.length())
+                .append(DEL)
+                .append(s);
+        }
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> tmp = new ArrayList<>();
+        for (int i = 0; i < str.length(); ) {
+            int j = i;
+            while (DEL != str.charAt(j)) {
+                j++;
+            }
+            j++;
+            int len = Integer.parseInt(str.substring(i, j-1));
+            tmp.add(str.substring(j, j + len));
+            i = j + len;
+        }
+        return List.copyOf(tmp);
+    }
+}
