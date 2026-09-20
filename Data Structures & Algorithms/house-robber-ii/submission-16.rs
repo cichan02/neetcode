ce632a1e024faafa07a474fb7bdae6079129581f
cichan <90219032+cichan02@users.noreply.github.com>
2026-09-20 21:42:14 +0300

    Add: house-robber-ii - submission-16

---
+++ Data Structures & Algorithms/house-robber-ii/submission-16.rs
@@ -0,0 +1,21 @@
+use std::cmp;
+
+impl Solution {
+    pub fn rob(nums: Vec<i32>) -> i32 {
+        let len: usize = nums.len();
+
+        nums[0]
+            .max(Self::rob_from_to(0, len - 1, &nums))
+            .max(Self::rob_from_to(1, len, &nums))
+    }
+
+    fn rob_from_to(start: usize, end: usize, nums: &Vec<i32>) -> i32 {
+        let (mut zero, mut first): (i32, i32) = (0, 0);
+        for i in start..end {
+            let tmp: i32 = first;
+            first = cmp::max(first, zero + nums[i]);
+            zero = tmp;
+        }
+        first
+    }
+}
