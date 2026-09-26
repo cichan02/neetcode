1c504c993f7fe698a8b8499887dca74a16418e34
cichan <90219032+cichan02@users.noreply.github.com>
2026-09-26 19:03:31 +0300

    Add: linked-list-cycle-detection - submission-7

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-7.rs
@@ -0,0 +1,33 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//     pub val: i32,
+//     pub next: *mut ListNode,
+// }
+//
+// impl ListNode {
+//     #[inline]
+//     pub fn new(val: i32) -> Self {
+//         ListNode { next: std::ptr::null_mut(), val }
+//     }
+// }
+
+impl Solution {
+    pub fn has_cycle(head: *mut ListNode) -> bool {
+        let mut slow = head;
+        let mut fast = head;
+
+        unsafe {
+            while !fast.is_null() && !(*fast).next.is_null() {
+                slow = (*slow).next;
+                fast = (*(*fast).next).next;
+
+                if slow == fast {
+                    return true;
+                }
+            }
+        }
+
+        false
+    }
+}
